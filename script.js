f17c1d9a8bcab5b9f93d566a6be2ea319abfda60
SebRiv96 <sebriveroa@gmail.com>
2021-11-08 15:02:37 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ 
+  $(".btn-close-right").click(function () {
+    $(".right-side").removeClass("show");
+    $(".expand-btn").addClass("show");
+  });
+  
+  $(".expand-btn").click(function () {
+    $(".right-side").addClass("show");
+    $(this).removeClass("show");
+  });
+});
